1d161fa19c9794cce2367f778999890397ce3b83
Ege Molu <egemolu@gmail.com>
2023-03-03 14:56:05 -0800

    Revert "Initial Commit"
    This reverts commit 32b4799642273e05dee12afb112a7fd884207cc9.

--- frontend/lib/utils/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const mainColor = Color(0xff343541);
-const chatBoxColor = Color(0xff3e3f4b);
-const userMessageBoxColor = Color(0xff343541);
-const chatGptMessageBoxColor = Color(0xff444654);
